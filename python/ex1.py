7b66c94473b081750751d72de022060d15425828
胡济豪 <jihao.hu@nanosic.com>
2016-01-21 15:22:48 +0800

    add exerceise for python

--- python/ex1.py
+++ python/ex1.py
@@ -1,1 +1,18 @@
 print "This is the first python file"
+print "This include a 'danyihao'"
+print 'This include a "shuangyinhao"'
+print "'#' means comments"
+
+print "I will now count my chickens"
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 %4
+print "Now I Will count the eggs:"
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+print 3 + 2 < 5 -7
+
+
+print "IS it greater?", 5 > -2
+print "Is it greater or equal ?", 5 >= 4 + 1
+print "Is it less or equal?", 5 <= -2
---
+++ python/ex6.py
@@ -0,0 +1,12 @@
+x = "There are %d types of people." %10
+binary = "binary"
+donot = "don't"
+y = "Those who knows %s and those who %s." %(binary, donot)
+
+print x
+print y
+
+print "I said %s." %(x)
+print "I also said %s" %y
+
+print "the x + y = %s" %(x+y)
