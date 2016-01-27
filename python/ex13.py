e4a0714b5cd03adf740ec897d1ec0e83ddac9018
胡济豪 <jihao.hu@nanosic.com>
2016-01-27 17:53:00 +0800

    add python exercise ex13.py ex14.py

---
+++ python/ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+v1, v2, v3, v4 = argv
+
+print "The script is called:", v1
+print "Your first variable is:", v2
+print "Your second variable is:", v3
+print "Your third variableis:", v4
