eda1df9cf09d79c50e62b62071677085446b5a24
jihao.hu <jihao.hu@nanosic.com>
2016-03-18 13:49:14 +0800

    add learn RecycleView-like List View

--- AndroidProject/RecyclerView_1/app/src/main/java/com/nanosic/recyclerview_1/MainActivity.java
+++ AndroidProject/RecyclerView_1/app/src/main/java/com/nanosic/recyclerview_1/MainActivity.java
@@ -1,41 +1,166 @@
 package com.nanosic.recyclerview_1;
 
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView = null;
     private LinearLayoutManager mLinearLayoutmanager = null;
+    private final String TAG = "MainActivity";
 
 
-    class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-        public String[] datas = null;
+    public class DividerItemDecoration extends RecyclerView.ItemDecoration {
+        private final int[] ATTRS = new int[]{
+                android.R.attr.listDivider
+        };
+        public final int HORIZONTAL_LIST = LinearLayoutManager.HORIZONTAL;
+        public final int VERTICAL_LIST = LinearLayoutManager.VERTICAL;
+        private Drawable mDivider;
+        private int mOrientation;
+
+        public DividerItemDecoration(Context context, int orientation) {
+            final TypedArray a = context.obtainStyledAttributes(ATTRS);
+            mDivider = a.getDrawable(0);
+            a.recycle();
+            setOrientation(orientation);
+        }
+
+        public void setOrientation(int orientation) {
+            mOrientation = orientation;
+        }
+
+        @Override
+        public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+//            super.onDraw(c, parent, state);
+            if (mOrientation == VERTICAL_LIST) {
+                drawVertical(c, parent);
+            } else {
+                drawHorizontal(c, parent);
+            }
+
+        }
+
+        public void drawVertical(Canvas c, RecyclerView parent) {
+            final int left = parent.getPaddingLeft();
+            final int right = parent.getWidth() - parent.getPaddingRight();
+            final int childCount = parent.getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                final View child = parent.getChildAt(i);
+                android.support.v7.widget.RecyclerView v = new android.support.v7.widget.RecyclerView(parent.getContext());
+                final RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+                final int top = child.getBottom() + params.bottomMargin;
+                final int bottom = top + mDivider.getIntrinsicHeight();
+                mDivider.setBounds(left, top, right, bottom);
+                mDivider.draw(c);
+            }
+        }
+
+        public void drawHorizontal(Canvas c, RecyclerView parent) {
+            final int top = parent.getPaddingTop();
+            final int bottom = parent.getHeight() - parent.getPaddingBottom();
+
+            final int childCount = parent.getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                final View child = parent.getChildAt(i);
+                final RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child
+                        .getLayoutParams();
+                final int left = child.getRight() + params.rightMargin;
+                final int right = left + mDivider.getIntrinsicHeight();
+                mDivider.setBounds(left, top, right, bottom);
+                mDivider.draw(c);
+            }
+        }
+
+        @Override
+        public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+            super.onDrawOver(c, parent, state);
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            if (mOrientation == VERTICAL_LIST) {
+                outRect.set(0, 0, 0, mDivider.getIntrinsicHeight());
+            } else {
+                outRect.set(0, 0, mDivider.getIntrinsicWidth(), 0);
+            }
+        }
+    }
+
+
+    class DataModel {
+    }
+
+    public static interface OnRecyclerViewItemClickListener {
+        void onItemClick(View view, DataModel data);
+    }
+
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> implements View.OnClickListener {
+        private OnRecyclerViewItemClickListener mOnItemClickListener = null;
+
+        public void setOnItemClickListener(OnRecyclerViewItemClickListener listener) {
+            this.mOnItemClickListener = listener;
+        }
+
+        public String[] dabs = null;
+
         public MyAdapter(String[] d) {
-            this.datas = d;
+            this.dabs = d;
         }
 
         @Override
-        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View
-            return null;
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+            ViewHolder vh = new ViewHolder(view);
+            view.setOnClickListener(this);
+            return vh;
         }
 
         @Override
-        public void onBindViewHolder(MyAdapter.ViewHolder holder, int position) {
-
+        public void onBindViewHolder(ViewHolder holder, int position) {
+//            Log.v(TAG, "holder=" + holder);
+//            Log.v(TAG, "position=" + sposition);
+            String str = this.dabs[position];
+            holder.tv.setText(str);
+            holder.tv.setTag(this.dabs[position]);
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            //Log.v(TAG, "this.dabs.length=" + this.dabs.length);
+            return this.dabs.length;
         }
 
-        class ViewHolder {
+        @Override
+        public void onClick(View v) {
+            if (mOnItemClickListener != null) {
+                mOnItemClickListener.onItemClick(v, (DataModel) v.getTag());
+            }
+        }
 
+        class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView tv;
+
+            public ViewHolder(View view) {
+                super(view);
+                tv = (TextView) view.findViewById(R.id.textview);
+            }
         }
     }
 
@@ -43,6 +168,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        mLinearLayoutmanager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLinearLayoutmanager);
+        mRecyclerView.setHasFixedSize(true);
+        String[] daces = new String[]{"1", "2", "1", "2", "1", "2", "1", "2", "1", "2", "1", "2", "1", "2", "1", "2", "1", "2", "1", "2",};
+        MyAdapter adapter = new MyAdapter(daces);
+        adapter.setOnItemClickListener(new OnRecyclerViewItemClickListener() {
+            @Override
+            public void onItemClick(View view, DataModel data) {
+                Log.i(TAG, "onItemClick");
+            }
+        });
+        mRecyclerView.setAdapter(adapter);
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
 
     }
 }
